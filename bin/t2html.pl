b826c4325d604ff8f91e2a0dafbb4193974a2485
Jari Aalto <jari.aalto@tamk.fi>
2007-11-08 15:42:46 +0200

    MakeMetaTags: Only if there is desc, output http-equiv
    Signed-off-by: Jari Aalto <jari.aalto@tamk.fi>

--- bin/t2html.pl
+++ bin/t2html.pl
@@ -7001,7 +7001,7 @@ sub MakeMetaTags ( % )
         push @ret, qq(  <$META="keywords"\n\tCONTENT="$kwd">\n\n);
     }
 
-    if ( defined $desc )
+    if ( $desc = /\S/ )
     {
         length($desc) > 1000
             and warn "$id: META DESC over 1000 characters";
