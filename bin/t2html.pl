cdce94dd65a82ea7bbae9088be23dd6805f91986
Jari Aalto <jari.aalto@cante.net>
2014-03-05 11:54:05 +0200

    (HandleCommandLineArgs): Remove defined() test from array
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- bin/t2html.pl
+++ bin/t2html.pl
@@ -78,7 +78,7 @@ use vars qw ( $VERSION );
 #   The following variable is updated by Emacs setup whenever
 #   this file is saved.
 
-$VERSION = '2014.0305.0951';
+$VERSION = '2014.0305.0952';
 
 # ****************************************************************************
 #
@@ -1036,7 +1036,7 @@ EOF
 	$BASE_URL = $1 if m,(.*)/,;
     }
 
-    if ( defined @CSS_FILE  and  @CSS_FILE )
+    if ( @CSS_FILE  and  @CSS_FILE )
     {
 	$JAVA_CODE = '';
 
